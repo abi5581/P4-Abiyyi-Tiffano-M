dc256e042afe98df53b7ac7eac51ec18199a407e
abi5581 <64617579+abi5581@users.noreply.github.com>
2020-05-12 14:57:38 +0800

    Create Jobsheet 2 C++ .cpp
    Mengisi dan menampilkan elemenr array;
    - program mengisi dan menampilkan elemen array dengan index array sesuai dengan NIM terakhir setiap Mahasiswa.

---
+++ Jobsheet 2 C++ .cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int x [4];
+	cout<<" masukkan nim 1 :";
+	cin>> x [1];
+	
+	cout<<" masukkan nim 2 :";
+	cin>> x [2];
+	
+	cout<<" masukkan nim 3 :";
+	cin>> x [3];
+	
+	cout<<" masukkan nim 4 :";
+	cin>> x [8];
+	
+	cout<<endl;
+	
+	cout<<" masukkan nim 1 = "<<x[1]<<endl;
+	cout<<" masukkan nim 2 = "<<x[2]<<endl;
+	cout<<" masukkan nim 3 = "<<x[3]<<endl;
+	cout<<" masukkan nim 4 = "<<x[8]<<endl;
+	system  ("PAUSE");
+}
