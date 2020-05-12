4c4ae970708d9d8c20964c39898979a2469d870f
abi5581 <64617579+abi5581@users.noreply.github.com>
2020-05-12 14:55:57 +0800

    Create Jobsheet 2 C.c
    Mengisi dan menampilkan elemenr array;
    - program mengisi dan menampilkan elemen array dengan index array sesuai dengan NIM terakhir setiap Mahasiswa.

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	printf("Nama     : Abiyyi Tiffano MP \nNim      : F1B019003\nKelompok : 1\n");
+	int x[4];
+    printf("Masukkan Akhiran Nim Mahasiswa 1: ");
+    scanf("%i",&x[0]);
+    printf("Nim Mahasiswa pertama = %i",x[0]);
+}
