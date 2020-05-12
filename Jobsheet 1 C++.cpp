a8e1cb332e102587e821efca3e98a6fd27d010f3
abi5581 <64617579+abi5581@users.noreply.github.com>
2020-05-12 14:53:54 +0800

    Create Jobsheet 1 C++.cpp
    Menampilkan array satu dimensi
    - program array satu dimensi yang menampilkan NIM masing-masing mahasiswa.

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main ()
+{	int my_array [4]= {1,2,3,8};
+out<<("data ke-1 =", my_array [0]);
+cout<<("data ke-2 =", my_array [1]);
+cout<<("data ke-3 =", my_array [2]);
+cout<<("data ke-4 =", my_array [3]);
+}
