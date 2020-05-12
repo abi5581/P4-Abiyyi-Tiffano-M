a4142748a1cc45c4bfa9cc091a66c3e4573109c1
abi5581 <64617579+abi5581@users.noreply.github.com>
2020-05-12 15:13:33 +0800

    Create Jobsheet 9 C .c
    Array sebagai karakter;
    - program array sebagai karakter dengan menampilkan nama setiap Mahasiswa

---
+++ Jobsheet 9 C .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+	int i;
+	char
+	nama[4]={'A','B','I','\0'};
+	char nama2 [4]="ABI";
+	printf("Array per karakter = ");
+	for(i=0;i<=3;i++)
+	{
+		printf("%c",nama[i]);
+	}
+	printf("\n");
+	printf("Array string  = %s\n\n",nama2);
+	return 0;
+}
